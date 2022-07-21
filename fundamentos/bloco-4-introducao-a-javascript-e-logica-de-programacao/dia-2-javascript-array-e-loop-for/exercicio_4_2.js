69ce5c7ffe7a3064f9840eb9dc2e1db8fb07ac8a
Weliton.lima <welitonlima159@gmail.com>
2022-07-21 15:54:08 -0300

    Exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4_2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4_2.js
@@ -12,10 +12,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //  }
 //  console.log(soma);
 
- // Ex 3
+ // Ex 3 e 4
  let soma=0;
  for(i=0;i<numbers.length; i+=1){
     soma+=numbers[i];
  }
  let media=soma/numbers.length;
  console.log(media);
+
+if(media>20){
+    console.log("valor maior que 20");
+}else{
+    console.log("valor menor ou igual a 20");
+}
