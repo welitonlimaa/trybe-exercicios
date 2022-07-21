8eb850c1c4a509948a095670d7645cdbc9da9391
Weliton.lima <welitonlima159@gmail.com>
2022-07-21 15:46:53 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4_2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4_2.js
@@ -1,5 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
- for(let number of numbers){
-    console.log(number);
+// Ex 1
+//  for(let number of numbers){
+//     console.log(number);
+//  }
+
+ //Ex 2
+ let soma=0;
+ for(i=0;i<numbers.length; i+=1){
+    soma+=numbers[i];
  }
+ console.log(soma);
+
